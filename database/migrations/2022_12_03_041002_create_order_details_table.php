42c9281a8e0b481000a12818df1765e378fccdd1
Lee Tien Sinh <muzankibut112@gmail.com>
2022-12-03 11:29:37 +0700

    Lab5 final

---
+++ database/migrations/2022_12_03_041002_create_order_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('quantity');
+            $table->float('price');
+            $table->timestamps();
+
+            $table->primary(['order_id', 'product_id']);
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table){
+            $table->dropForeign('order_details_order_id_foreign');
+            $table->dropForeign('order_details_product_id_foreign');
+        });
+        Schema::dropIfExists('order_details');
+    }
+};
